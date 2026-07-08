906dc6b57f2da24471db9a98523d9d476d1c8647
Nguyen <nguyenchiennguyen98@gmail.com>
2026-07-08 22:14:01 +0700

    Nguyen 8-7

--- WebBanHang.DAL/obj/Debug/net8.0/WebBanHang.DAL.AssemblyInfo.cs
+++ WebBanHang.DAL/obj/Debug/net8.0/WebBanHang.DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanHang.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+824a16fd7aa5af760203f1d8a8fcc7bd8bb1b640")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62a16ada4d16421694ce6f3d95364e4aeaaaef49")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanHang.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanHang.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
