84b2664db8553328b469796b0244cd0ae21c801c
Nguyen <nguyenchiennguyen98@gmail.com>
2026-07-08 23:01:25 +0700

    change appsetting

---
+++ WebBanHang.DAL/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebBanHang.DAL.Models;
+
+public partial class Category
+{
+    public int CategoryId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public int? ParentId { get; set; }
+
+    public string? Status { get; set; }
+
+    public virtual ICollection<Category> InverseParent { get; set; } = new List<Category>();
+
+    public virtual Category? Parent { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
---
+++ WebBanHang.DAL/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebBanHang.DAL.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string FullName { get; set; } = null!;
+
+    public string Role { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime? DeletedDate { get; set; }
+
+    public virtual ICollection<Cart> Carts { get; set; } = new List<Cart>();
+
+    public virtual Customer? Customer { get; set; }
+
+    public virtual ICollection<InventoryTransaction> InventoryTransactions { get; set; } = new List<InventoryTransaction>();
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
