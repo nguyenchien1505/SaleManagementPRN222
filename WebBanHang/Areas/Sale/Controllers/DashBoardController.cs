9a591815df4df3facb355e72bd77e508870b041f
Nguyen <nguyenchiennguyen98@gmail.com>
2026-07-24 18:15:16 +0700

    Save work

--- WebBanHang/Areas/Sale/Controllers/DashBoardController.cs
+++ WebBanHang/Areas/Sale/Controllers/DashBoardController.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
 using WebBanHang.DAL.Context;
 
 namespace WebBanHang.Areas.Sale.Controllers
